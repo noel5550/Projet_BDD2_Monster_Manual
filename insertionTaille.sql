b7fe3ef0c391c4923311b6793bcc3ced5bb73d67
HacquesF <florian.hacques@etu.univ-nantes.fr>
2017-03-07 23:00:59 +0100

    Ajout insertion des tailles et xp/chal

---
+++ insertionTaille.sql
@@ -0,0 +1,8 @@
+--Insertions dans la table Taille
+INSERT INTO Taille VALUES ('Tiny', 4);
+INSERT INTO Taille VALUES ('Small', 6);
+INSERT INTO Taille VALUES ('Medium', 8);
+INSERT INTO Taille VALUES ('Large', 10);
+INSERT INTO Taille VALUES ('Huge', 12);
+INSERT INTO Taille VALUES ('Gargantuan', 20);
+commit;
