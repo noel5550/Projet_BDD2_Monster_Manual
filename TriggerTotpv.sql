8b20d4f1f4873d1f0c550ccff94d098922e09197
Florian HACQUES <E135442U@I019V7pc19.ensinfo.sciences.univ-nantes.prive>
2017-03-08 17:54:40 +0100

    Le trigger de point de vie

---
+++ TriggerTotpv.sql
@@ -0,0 +1,23 @@
+SET SERVEROUTPUT ON;
+--Trigger mettant a jour l'attribut PV
+CREATE OR REPLACE TRIGGER totPV_bef_ins_cara
+       BEFORE INSERT ON Caracteristique
+       FOR EACH ROW
+DECLARE
+	vie Caracteristique.PV%Type;
+	de Taille.dePV%TYPE;
+BEGIN
+	vie:=0;
+	--Recupere la valeur du de
+	SELECT dePV INTO de
+	       FROM Taille
+	       WHERE taille=:new.taille;
+	--Lancement des des
+	FOR i IN 1..:new.multiplePV LOOP
+	    vie := vie+DBMS_RANDOM.RANDOM(1,de+1);
+	END LOOP;
+	--Retour des points de vues avec l ajout de la constante
+	:new.PV:= vie + :new.constantePV;	
+END totPV;
+/
+SHOW ERROR;
